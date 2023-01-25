9366393133b001d2c269dec0b8e84570a24833bb
Thalyson Dornelas Gomes <115152170+ThalysonD@users.noreply.github.com>
2023-01-24 21:14:03 -0300

    Add files via upload

---
+++ Comando_For_cubo/src/application/Program.java
@@ -0,0 +1,25 @@
+package application;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+
+			int primeiro = i;
+			int segundo = i * i;
+			int terceiro = i * i * i;
+			System.out.printf("%d %d %d%n", primeiro, segundo, terceiro);
+		}
+
+		sc.close();
+
+	}
+
+}
